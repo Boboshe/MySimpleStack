a317cce460f5a68e210cc7781e23ece0319cb348
Boris <boboshesam@gmail.com>
2015-03-17 16:18:26 +0100

    "Validation du .gitignore et de l'ensemble de fichiers TP2-EXO2-Q3"

---
+++ src/main/java/dcll/bter/mysimplestack/App.java
@@ -0,0 +1,24 @@
+/**
+ * Package de moi par défaut xD
+ */
+package dcll.bter.mysimplestack;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    /**
+     *
+     */
+    protected App() {
+        // prevents calls from subclass
+        throw new UnsupportedOperationException();
+    }
+
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+        //Normalement j'ai bien fait le CheckStyle => Aucune Violation de règle
+    }
+}
